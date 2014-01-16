adfc18d28ace33489951efdff6aadb154ffecd7c
Kimberly Narine <kimberlyonarine@gmail.com>
2014-01-16 00:41:54 -0500

    finishing touches --chrome

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(function() {
     $("section.spanish").hide()
 
     //SWITCH LANGUAGES
-    $("nav a").on("click", function (event) {
+    var changeSection = function (event) {
       event.preventDefault();
       $("section").hide();
       $("section."+this.className).show();
@@ -14,8 +14,18 @@ $(function() {
        $("body > div").addClass(this.className)
        
       document.title = document.title.substring(0,24) + " " + this.className;
-    });
+    };
 
+    $("a.english").click(changeSection);
+    $("a.spanish").click(changeSection);
+
+    var makeBlink = function (domObject, firstColor, secondColor) {
+        $(domObject).css({"border-color":firstColor})
+        setTimeout(function(){$(domObject).css({"border-color":secondColor})}, 300)
+        setTimeout(function(){$(domObject).css({"border-color":firstColor})}, 600)
+        setTimeout(function(){$(domObject).css({"border-color":secondColor})}, 900)
+    }
+    
     //PLAY SOUND
     $("section > a").on("click", function (event) {
       event.preventDefault();
@@ -24,13 +34,23 @@ $(function() {
       words = $("input").val();
   
       if (words == ""){
-        alert("put something")
+          //make input field blink
+          makeBlink('input', "red", "rgba(33, 171, 205, .5)")
+      }
+      else if (typeof InstallTrigger !== 'undefined'){
+        //Firefox   
       }
       else {
-        $(this.firstChild).attr("src", "http://translate.google.com/translate_tts?tl="+accent+"&q="+words)
-        this.firstChild.play()
-        console.log("http://translate.google.com/translate_tts?tl="+accent+"&q="+words)
+        $('footer').empty();
+        $('footer').append('<iframe src="http://translate.google.com/translate_tts?tl=' + accent + '&q=' + words + '" style="display:none;"></iframe>')
       };
 
+    });
+    
+    //DISABLE FORM SUBMIT (HITTING ENTER KEY IN INPUT AREA)
+    $('form').on("submit", function (event) {
+        event.preventDefault();
+        console.log("here")
+        makeBlink('section > a',"white", "#FF9966")
     });
 });
